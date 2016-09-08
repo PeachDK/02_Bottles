d418274e31569c10698e994f57c5aff54381aabb
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-08 11:25:23 +0200

    Done assigment by creating a method with switch case.

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -1,9 +1,80 @@
-﻿namespace _02_Bottles
+﻿using System;
+namespace _02_Bottles
 {
     internal class Song
     {
-        public Song()
+        public string CountBottles(int v)
         {
+            switch (v)
+            {
+
+
+                case 0:
+
+                    return " No more bottles of beer on the wall.";
+
+                    break;
+
+                case 1:
+
+                    return "1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+                
+                    break;
+
+
+                case 2:
+
+                    return "2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+
+                    break;
+
+
+                case 3:
+
+                    return "3 bottles of beer on the wall."
+                  + " 3 bottles of beer."
+                  + " Take one down and pass it around."
+                  + " 2 bottles of beer on the wall."
+                  + " 2 bottles of beer on the wall."
+                  + " 2 bottles of beer."
+                  + " Take one down and pass it around."
+                  + " 1 bottle of beer on the wall."
+                  + " 1 bottle of beer on the wall."
+                  + " 1 bottle of beer."
+                  + " Take one down and pass it around."
+                  + " No more bottles of beer on the wall.";
+
+                    break;
+
+                default:
+
+                  
+
+                    break;
+
+
+            }
+
+
+
+            return "0";
+
+            
+
         }
+
+
+
+
     }
 }
